abfc36dfd1dfebff6648593ee621b709ade16084
MaxxWell <maxxwell131@yahoo.com>
2018-02-25 17:27:03 +0200

    first commit

---
+++ AdditionalTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

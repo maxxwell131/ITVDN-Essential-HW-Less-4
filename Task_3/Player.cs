bca53b102c25785fe236b9b5987d1a6b83973913
maxxwell131 <maxxwell131@yahoo.com>
2018-02-28 20:33:01 +0200

    Task 3 first commit

---
+++ Task_3/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Player : IRecodable, IPlayable
+    {
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("IRecodable.Pause()");
+        }
+
+        void IRecodable.Record()
+        {
+            Console.WriteLine("IRecodable.Record()");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("IRecodable.Stop()");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("IPlayable.Pause()");
+        }
+
+        void IPlayable.Play()
+        {
+            Console.WriteLine("IPlayable.Play()");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("IPlayable.Stop()");
+        }
+    }
+}
